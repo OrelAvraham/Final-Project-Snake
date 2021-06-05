d6801b179d5823ef3c02e4eb95e7d106f3449bc8
orel <orel.avr95@gmail.com>
2021-06-05 11:45:29 +0300

    Created separate file for the recorder ~Orel

--- snake/main.py
+++ snake/main.py
@@ -1,38 +1,6 @@
 from snake.game import Game
 from snake.players import *
-import os
-
-
-class Recorder:
-    # TODO: use Iziks technic of saving long list of the snake and lengths instead of saving the snake of each turn
-    def __init__(self, snake, food, reward=0):
-        self.snakes = [snake]
-        # self.lengths = [len(snake)]
-        self.foods = [food]
-        self.rewards = [reward]
-
-    def update(self, snake, food, reward):
-        self.snakes.append(snake)
-        # self.lengths.append(len(snake))
-        self.foods.append(food)
-        self.rewards.append(reward)
-
-    def hard_save(self, game_name=None, dir_name='game_history'):
-        if not game_name:
-            n = len(os.listdir(f'../game_viewer/{dir_name}/'))
-            game_name = f'Game{n}'
-        path = f'../game_viewer/{dir_name}/{game_name}.RAZ'
-        while os.path.exists(path):
-            name = input('Name already exists, enter another one:')
-            path = f'{name}.snake'
-
-        with open(path, 'w') as f:
-            f.write(str(self.snakes))
-            f.write('\n')
-            f.write(str(self.foods))
-            f.write('\n')
-            f.write(str(self.rewards))
-
+from snake.recorder import Recorder
 
 def main():
     snake_game: Game = Game()
